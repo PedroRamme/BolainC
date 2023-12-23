16b349e051d2775542a554ff4a06113f6e101cd7
Pedro Ramme <142846873+PedroRamme@users.noreply.github.com>
2023-12-22 21:42:57 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<graphics.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <graphics.h>
 int main()
 {
     int gd = DETECT, gm;
@@ -8,34 +8,32 @@ int main()
     int tamanhoXMax = getmaxx();
     int y = 50;
 
-    while(1){
+    while (1)
+    {
         setcolor(WHITE);
-        line(0,400,tamanhoXMax,400);
-    
-        if(y >300){
+        line(0, 400, tamanhoXMax, 400);
+
+        if (y > 300)
+        {
             circle(50, 370, 30);
         }
-        for(int i = y; i<370; i++){
+        for (int i = y; i < 370; i++)
+        {
             setcolor(WHITE);
             circle(50, i, 30);
             delay(1);
             setcolor(BLACK);
             circle(50, i, 30);
-    
-    
-    
         }
-        for(int j = 370; j>y; j--){
+        for (int j = 370; j > y; j--)
+        {
             setcolor(WHITE);
             circle(50, j, 30);
             delay(1);
             setcolor(BLACK);
             circle(50, j, 30);
-    
-    
         }
         y += 50;
-
     }
 
     delay(500000);
