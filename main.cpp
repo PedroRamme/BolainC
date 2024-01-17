ad72f8549dc21b0aff1df8a48029df4d689ea9d7
Pedro Ramme <pedroh.ramme@gmail.com>
2024-01-17 14:42:26 -0300

    nao sei qual versao ja ta

--- main.cpp
+++ main.cpp
@@ -1,43 +1,68 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<graphics.h>
-int main()
-{
+#include<windows.h>
+
+//GetAsyncKeyState(VK_UP)
+
+void paintCircle(int i, int color) {
+    setcolor(color);
+    circle(50, i, 30);
+}
+
+void drawCircle(int i) {
+    paintCircle(i, WHITE);
+}
+
+void eraseCircle(int i) {
+    paintCircle(i, BLACK);
+}
+
+void updateCircle(int i) {
+    drawCircle(i);
+    delay(1);
+    eraseCircle(i);
+}
+
+
+void init() {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, NULL);
+}
+
+int main()
+{
+    init();
+
     int tamanhoXMax = getmaxx();
     int y = 50;
+    int d = 1;
 
-    while(1){
+
+    setcolor(WHITE);
     line(0,400,tamanhoXMax,400);
 
-    if(y >300){
-        circle(50, 370, 30);
-    }
-    for(int i = y; i<370; i++){
-        setcolor(WHITE);
-        circle(50, i, 30);
-        delay(1);
-        setcolor(BLACK);
-        circle(50, i, 30);
+    while(true){
+
+        if(y >300){
+            drawCircle(y);
+        }
+
+        for(int i = y; i<370; i++){
+            updateCircle(i);
+        }
 
 
+        for(int i= 370; i>y; i--){
+            updateCircle(i);
+        }
 
-    }
-    for(int j = 370; j>y; j--){
-        setcolor(WHITE);
-        circle(50, j, 30);
-        delay(1);
-        setcolor(BLACK);
-        circle(50, j, 30);
-
+        y += 50;
 
     }
-    y += 50;
-
-    }
-
+    printf("saiu");
     delay(500000);
     closegraph();
     return 0;
 }
+
